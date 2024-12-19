5a8138529f8a1f9f7b0b582fccd738b186111c07
Takahashi1319 <jugashivuiri@gmail.com>
2024-12-19 16:22:47 +0900

    feat:minecraft

---
+++ src/app/minecraft/1stBranch/page.client.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import HomeCard from "@/components/card/card";
+
+const Minecraft1stBranchPage = () => {
+  const buttons = [
+    { url: "/minecraft/2ndBranch/login", label: "サーバーに入れない" },
+    { url: "/others/1stBranch", label: "バージョンを知りたい" },
+    { url: "/others/1stBranch", label: "アドレスを知りたい" },
+    { url: "/others/1stBranch", label: "その他" },
+  ];
+
+  return (
+    <HomeCard
+      title={"minecraftに関するヘルプ"}
+      subTitle={"サポートが必要な項目をお選びください"}
+      buttons={buttons}
+    />
+  );
+};
+
+export default Minecraft1stBranchPage;
---
+++ src/app/minecraft/1stBranch/page.tsx
@@ -0,0 +1,7 @@
+import Minecraft1stBranchPage from "./page.client";
+
+const page = () => {
+  return <Minecraft1stBranchPage />;
+};
+
+export default page;

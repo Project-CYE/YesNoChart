347ccd8fd3a7f0c6427eaae9773c1d9767d02f7e
Takahashi1319 <jugashivuiri@gmail.com>
2024-12-20 16:16:44 +0900

    hotfix:config

---
+++ src/app/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-empty-object-type": "off",
+  },
+};
